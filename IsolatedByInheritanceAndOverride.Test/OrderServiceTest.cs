8353d67989f6df14d337b3c3c056433b37a43aad
TITANSOFT\sam.chuang95 <sam.chuang@titansoft.com.sg>
2019-01-12 16:47:33 +0800

    build success moq Test_SyncBookOrders_3_Orders_Only_2_book_order

--- IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
+++ IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Moq;
+using Moq.Protected;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace IsolatedByInheritanceAndOverride.Test
 {
@@ -11,10 +15,44 @@ namespace IsolatedByInheritanceAndOverride.Test
         [Test]
         public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
         {
+            var mockOrderService = new Mock<OrderService>();
+            mockOrderService.Protected().Setup<List<Order>>("GetOrders").Returns(new List<Order>
+            {
+                new Order
+                {
+                    Type = "Book"
+                },
+                new Order
+                {
+                    Type = "Book"
+                },
+                new Order
+                {
+                    Type = "no"
+                }
+            });
+            
+            mockOrderService.Protected().Setup<IBookDao>("GetBookDao").Returns(new FakeBookDao());
+
+            mockOrderService.Object.SyncBookOrders();
+
             // hard to isolate dependency to unit test
 
             //var target = new OrderService();
             //target.SyncBookOrders();
+        }
+    }
+
+    public class FakeBookDao : IBookDao
+    {
+        public void Insert(Order order)
+        {
+            // directly depend on some web service
+            //var client = new HttpClient();
+            //var response = client.PostAsync<Order>("http://api.joey.io/Order", order, new JsonMediaTypeFormatter()).Result;
+
+            //response.EnsureSuccessStatusCode();
+            //throw new NotImplementedException();
         }
     }
 }
